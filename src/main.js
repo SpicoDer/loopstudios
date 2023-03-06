3d2f2dc254a73206f370560c06361495e0af6038
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-06 21:14:00 +0800

    initial commit.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
-const menu = document.querySelector('#menu');
-const menuLinks = document.querySelector('#menu-links');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav');
+const navSection = document.querySelector('#nav-section');
+const contact = document.querySelector('#contact-btn');
+const header = document.querySelector('header');
 
-menu.addEventListener('click', () => {
-  menu.classList.toggle('open');
-  menuLinks.classList.toggle('disable');
-  menuLinks.classList.toggle('enable');
+// Scroll padding
+const navHeight = nav.offsetHeight;
+document.documentElement.style.setProperty(
+  '--scroll-padding',
+  navHeight + 'px'
+);
+
+// State of menu
+menuBtn.addEventListener('click', () => {
+  menuBtn.toggleAttribute('data-open');
+  menu.classList.toggle('disable');
+  menu.classList.toggle('enable');
 });
+
+// Scrolling effect
+
+const rect = header.getBoundingClientRect();
+
+window.onscroll = function () {
+  navSection.classList.add('bg-prim-600');
+  contact.classList.add('bg-prim-400');
+
+  if (window.scrollY < 1) {
+    navSection.classList.remove('bg-prim-600');
+    contact.classList.add('bg-prim-400');
+  }
+
+  if (window.scrollY > rect.height) {
+    menu.classList.add('disable');
+    menu.classList.remove('enable');
+    menuBtn.removeAttribute('data-open');
+  }
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,24 +9,24 @@ module.exports = {
       xl: '1440px',
     },
     fontFamily: {
-      heading: ['Alata', 'sans-serif'],
-      body: ['Josefin Sans', 'sans-serif'],
+      // heading: ['Alata', 'sans-serif'],
+      body: ['Quicksand', 'sans-serif'],
     },
     extend: {
-      // colors: {
-      //   prim: {
-      //     200: '#2c89ac',
-      //     300: '#4496b5',
-      //     400: '#26bba4',
-      //     500: '#116382',
-      //     600: '#0d4a62',
-      //   },
-      //   sec: '#818fff',
-      //   txt: {
-      //     dark: '#212529',
-      //     light: '#666',
-      //   },
-      // },
+      colors: {
+        prim: {
+          200: '#d1bdaa',
+          300: '#ae8c6b',
+          400: '#8b5a2b',
+          500: '#613f1e',
+          600: '#2a1b0d',
+        },
+        acc: '#add8e6',
+        txt: {
+          dark: '#333333',
+          light: '#5c5c5c',
+        },
+      },
     },
   },
   plugins: [],
