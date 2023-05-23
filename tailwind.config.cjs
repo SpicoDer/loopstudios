f11bcb737292b976c2ffd5be6567073fa2931de0
spicoder <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-05-23 18:38:18 +0800

    Create navigation component.

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      // heading: [],
+      body: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        prim: {
+          200: "#2c89ac",
+          300: "#4496b5",
+          400: "#26bba4",
+          500: "#116382",
+          600: "#0d4a62",
+        },
+        //   sec: '#818fff',
+        txt: {
+          dark: "#212529",
+          light: "#666",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
